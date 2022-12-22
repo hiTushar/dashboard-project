862743de5d941675280409f5cf17337a757d1f5e
Tushar Jain <tushar.jain@getvymo.com>
2022-12-22 18:50:06 +0530

    Add barebones data table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-export default function Table() {
+export default function Table(props) {
+    const { tableData, tableColumns } = props;
+
+    const getTableHead = (col, key) => <th key={key} className="table-col">{col}</th>
+    
+    const getTableRow = (row, key) => <tr key={key} className="table-row">{Object.values(row).map(cell => getTableCell(cell, row.id))}</tr>
+
+    const getTableCell = (cell, key) => <td key={`${cell}${key}`} className="table-cell">{cell}</td>
+
     return (
-        <></>
+        <table>
+            <thead>
+                {tableColumns.map(col => getTableHead(col, col))}
+            </thead>
+            <tbody>
+                {tableData.map(row => getTableRow(row, row.id))}
+            </tbody>
+        </table>
     )
 }
