2c56f4e9ae6d0ae300f17e850844f16c4c0ecdc8
Tushar Jain <tushar.jain@getvymo.com>
2022-12-23 00:40:59 +0530

    Add pagination \n there's glitch in dropdown + styling changes are missin

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Pagination from "./Pagination";
 import Table from "./Table";
 
+const DEFAULT_ROWS_PER_PAGE = 15;
+
 export default function Dashboard(props) {
     const [ data, setData ] = useState(props.data)
+    const [ tableData, setTableData ] = useState([]);
+    const [ rowsPerPage, setRowsPerPage ] = useState(DEFAULT_ROWS_PER_PAGE);
+    const [ pageNo, setPageNo ] = useState(0);
 
-    console.log(data);
+    useEffect(() => {
+        console.log({ rowsPerPage, pageNo });
+        let firstRowIndex = rowsPerPage * pageNo;
+        let lastRowIndex = rowsPerPage * pageNo + rowsPerPage;
+        setTableData(data.slice(firstRowIndex, lastRowIndex));
+    }, [rowsPerPage, pageNo]);
+
     return (
         <>
-            <Table />
-            <Pagination />
+            {
+                tableData.length ? (
+                    <>
+                        <Table 
+                            tableData={tableData} 
+                            tableColumns={Object.keys(tableData[0])}
+                        />
+                        <Pagination 
+                            setRowsPerPage={setRowsPerPage}
+                            setPageNo={setPageNo}
+                            pageNo={pageNo}
+                            totalRows={data.length}
+                            rowsPerPage={rowsPerPage}
+                        />
+                    </>
+                ) : null
+            }
         </>
     )
 }
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,7 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Pagination() {
+const DEFAULT_PAGINATION_RANGE = 5; 
+const DEFAULT_PAGINATION_STEP = 1;
+const ROWS_PER_PAGE_OPTIONS = [ 15, 30, 60 ];
+
+export default function Pagination(props) {
+    const { setRowsPerPage, setPageNo, pageNo, totalRows, rowsPerPage } = props;
+    const [ paginationRange, setPaginationRange ] = useState(Array.from({ length: 5 }, (v, i) => i));
+
+    const totalPages = parseInt(totalRows / rowsPerPage) + ((totalRows % rowsPerPage) ? 1 : 0);
+    
+    const checkDisabled = (value) => {
+        switch(value) {
+            case '<': 
+                if(pageNo === 0) return true;
+                break;
+
+            case '>':
+                if(pageNo === (totalPages - 1)) return true;
+                break;
+
+            case '<<': 
+                if((pageNo - DEFAULT_PAGINATION_RANGE) < 0) return true;
+                break;
+
+            case '>>':
+                if((pageNo + DEFAULT_PAGINATION_RANGE) > (totalPages - 1)) return true;
+                break;
+        }
+        return false;
+    }
+
+    const changePage = (value) => {
+        let nextPage;
+        switch(value) {
+            case '<': 
+                nextPage = pageNo - DEFAULT_PAGINATION_STEP;
+                if (nextPage < paginationRange[0]) {
+                    calculatePaginationRange(-DEFAULT_PAGINATION_STEP);
+                }
+                break;
+
+            case '>':
+                nextPage = pageNo + DEFAULT_PAGINATION_STEP;
+                if (nextPage > paginationRange[paginationRange.length - 1]) {
+                    calculatePaginationRange(DEFAULT_PAGINATION_STEP);
+                }
+                break;
+
+            case '<<': 
+                nextPage = pageNo - DEFAULT_PAGINATION_RANGE;
+                calculatePaginationRange(-DEFAULT_PAGINATION_RANGE);
+                break;
+
+            case '>>':
+                nextPage = pageNo + DEFAULT_PAGINATION_RANGE;
+                calculatePaginationRange(DEFAULT_PAGINATION_RANGE);
+                break;
+
+            default:
+                nextPage = value;  // pages start from 0 index
+                break;
+            }
+            setPageNo(nextPage);
+    }
+
+    const calculatePaginationRange = (step) => {
+        let pagesForPagination = [];
+        let startPage = step !== null ? paginationRange[0] + step : 0;
+        for(let i = startPage; i < startPage + DEFAULT_PAGINATION_RANGE; i++) {
+            pagesForPagination.push(i);
+        }
+        setPaginationRange(pagesForPagination);
+    }
+
+    const changeRowsPerPage = (value) => {
+        setRowsPerPage(value);
+        changePage(0);
+    }
+
     return (
-        <></>
+        <div className="pagination">
+            <button  
+                onClick={(e) => changePage(e.target.textContent)} 
+                disabled={checkDisabled('<<')}
+            >
+                {'<<'}
+            </button>
+            <button  
+                onClick={(e) => changePage(e.target.textContent)}
+                disabled={checkDisabled('<')}
+            >
+                {'<'}
+            </button>
+            {paginationRange.map(button => <button key={button} onClick={(e) => changePage(e.target.textContent - 1)}>{Number(button) + 1}</button>)}
+            <button  
+                onClick={(e) => changePage(e.target.textContent)}
+                disabled={checkDisabled('>')}
+            >
+                {'>'}
+            </button>
+            <button  
+                onClick={(e) => changePage(e.target.textContent)}
+                disabled={checkDisabled('>>')}
+            >
+                {'>>'}
+            </button>
+            <select value={rowsPerPage} onChange={(e) => changeRowsPerPage(parseInt(e.target.value))}>
+                {ROWS_PER_PAGE_OPTIONS.map(opt => <option key={opt}>{opt}</option>)}
+            </select>
+        </div>
     )
 }
